b613bda0574db50d4aca09b0acc739a9afe4f68d
Loïc Mahieu <mahieuloic@gmail.com>
2019-04-30 11:10:53 +0200

    react-admin-provider: fix

--- packages/react-admin-provider/src/dataProvider.ts
+++ packages/react-admin-provider/src/dataProvider.ts
@@ -1,15 +1,11 @@
-import { ProviderFileListOptions } from "@react-admin-git-provider/common";
-import { GitlabProviderFileList as BaseGitlabProviderFileList } from "@react-admin-git-provider/gitlab";
-import { GitlabOptions } from "@react-admin-git-provider/gitlab/lib/GitlabProviderAPI";
-import { Omit } from "type-fest";
+import {
+  GitlabProviderFileList as BaseGitlabProviderFileList,
+  GitlabProviderFileListOptions,
+} from "@react-admin-git-provider/gitlab";
 import { getToken } from "./authProvider";
 
 export class GitlabProviderFileList extends BaseGitlabProviderFileList {
-  constructor(
-    options: ProviderFileListOptions & {
-      gitlabOptions: GitlabOptions // Omit<GitlabOptions, "oauthToken">;
-    },
-  ) {
+  constructor(options: GitlabProviderFileListOptions) {
     super({
       ...options,
       gitlabOptions: {

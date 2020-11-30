23eee0680f966a8d1e91e3966b9ef5c0b7c749d8
Loïc Mahieu <mahieuloic@gmail.com>
2020-11-30 18:20:08 +0100

    server: backend: gitlab: prepare commit for indent change

--- packages/server/src/backend/gitlab.ts
+++ packages/server/src/backend/gitlab.ts
@@ -39,228 +39,230 @@ export interface IBackendGitlabOptions {
   privateToken: string;
 }
 
-export const backendGitlab = (options: IBackendGitlabOptions): IBackend => ({
-  async tree({ projectId, page, path, ref }: IBackendTreeOptions) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(projectId)}/repository/tree?` +
-        querystring.stringify({
-          page,
-          path,
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+export const backendGitlab = (options: IBackendGitlabOptions): IBackend => {
+  return {
+    async tree({ projectId, page, path, ref }: IBackendTreeOptions) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(projectId)}/repository/tree?` +
+          querystring.stringify({
+            page,
+            path,
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          timeout: options.timeout,
         },
-        json: true,
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
-  async readFile({ projectId, ref, file }: IBackendReadFileOptions) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(
-        projectId,
-      )}/repository/files/${encodeURIComponent(file)}?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
+    async readFile({ projectId, ref, file }: IBackendReadFileOptions) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(
+          projectId,
+        )}/repository/files/${encodeURIComponent(file)}?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          timeout: options.timeout,
         },
-        json: true,
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, [
-        ...basePickHeaders,
-        "x-gitlab-blob-id",
-        "x-gitlab-commit-id",
-        "x-gitlab-content-sha256",
-        "x-gitlab-encoding",
-        "x-gitlab-file-name",
-        "x-gitlab-file-path",
-        "x-gitlab-last-commit-id",
-        "x-gitlab-ref",
-        "x-gitlab-size",
-      ]),
-    };
-  },
-  async readFileRaw({ projectId, ref, file }: IBackendReadFileOptions) {
-    return got.stream(
-      `/projects/${encodeURIComponent(
-        projectId,
-      )}/repository/files/${encodeURIComponent(file)}/raw?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+      return {
+        body,
+        headers: pick(headers, [
+          ...basePickHeaders,
+          "x-gitlab-blob-id",
+          "x-gitlab-commit-id",
+          "x-gitlab-content-sha256",
+          "x-gitlab-encoding",
+          "x-gitlab-file-name",
+          "x-gitlab-file-path",
+          "x-gitlab-last-commit-id",
+          "x-gitlab-ref",
+          "x-gitlab-size",
+        ]),
+      };
+    },
+    async readFileRaw({ projectId, ref, file }: IBackendReadFileOptions) {
+      return got.stream(
+        `/projects/${encodeURIComponent(
+          projectId,
+        )}/repository/files/${encodeURIComponent(file)}/raw?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          timeout: options.timeout,
         },
-        timeout: options.timeout,
-      },
-    );
-  },
-  async headFile({ projectId, ref, file }: IBackendReadFileOptions) {
-    const { headers } = await got(
-      `/projects/${encodeURIComponent(
-        projectId,
-      )}/repository/files/${encodeURIComponent(file)}?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+      );
+    },
+    async headFile({ projectId, ref, file }: IBackendReadFileOptions) {
+      const { headers } = await got(
+        `/projects/${encodeURIComponent(
+          projectId,
+        )}/repository/files/${encodeURIComponent(file)}?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          method: "head",
+          timeout: options.timeout,
         },
-        json: true,
-        method: "head",
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      headers: pick(headers, [
-        ...basePickHeaders,
-        "x-gitlab-blob-id",
-        "x-gitlab-commit-id",
-        "x-gitlab-content-sha256",
-        "x-gitlab-encoding",
-        "x-gitlab-file-name",
-        "x-gitlab-file-path",
-        "x-gitlab-last-commit-id",
-        "x-gitlab-ref",
-        "x-gitlab-size",
-      ]),
-    };
-  },
-  async commit({ projectId, commitBody }: IBackendCommitOptions) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(projectId)}/repository/commits`,
-      {
-        baseUrl: getBaseUrl(options),
-        body: commitBody,
-        headers: {
-          "Private-Token": options.privateToken,
+      return {
+        headers: pick(headers, [
+          ...basePickHeaders,
+          "x-gitlab-blob-id",
+          "x-gitlab-commit-id",
+          "x-gitlab-content-sha256",
+          "x-gitlab-encoding",
+          "x-gitlab-file-name",
+          "x-gitlab-file-path",
+          "x-gitlab-last-commit-id",
+          "x-gitlab-ref",
+          "x-gitlab-size",
+        ]),
+      };
+    },
+    async commit({ projectId, commitBody }: IBackendCommitOptions) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(projectId)}/repository/commits`,
+        {
+          baseUrl: getBaseUrl(options),
+          body: commitBody,
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          method: "POST",
+          timeout: options.timeout,
         },
-        json: true,
-        method: "POST",
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
 
-  async listPipelines({ projectId, ref }: IBackendListPipelinesOptions) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(projectId)}/pipelines?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+    async listPipelines({ projectId, ref }: IBackendListPipelinesOptions) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(projectId)}/pipelines?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          timeout: options.timeout,
         },
-        json: true,
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
 
-  async triggerPipeline({ projectId, ref }: IBackendListPipelinesOptions) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(projectId)}/pipeline?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+    async triggerPipeline({ projectId, ref }: IBackendListPipelinesOptions) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(projectId)}/pipeline?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          method: "POST",
+          timeout: options.timeout,
         },
-        json: true,
-        method: "POST",
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
 
-  async getPipeline({
-    projectId,
-    id,
-  }: IBackendBaseOptions & {
-    id: string;
-  }) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(projectId)}/pipelines/${id}`,
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+    async getPipeline({
+      projectId,
+      id,
+    }: IBackendBaseOptions & {
+      id: string;
+    }) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(projectId)}/pipelines/${id}`,
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          timeout: options.timeout,
         },
-        json: true,
-        timeout: options.timeout,
-      },
-    );
+      );
 
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
 
-  async showBranch({ projectId, ref }) {
-    const { body, headers } = await got(
-      `/projects/${encodeURIComponent(
-        projectId,
-      )}/repository/branches/${encodeURIComponent(ref)}?` +
-        querystring.stringify({
-          ref,
-        }),
-      {
-        baseUrl: getBaseUrl(options),
-        headers: {
-          "Private-Token": options.privateToken,
+    async showBranch({ projectId, ref }) {
+      const { body, headers } = await got(
+        `/projects/${encodeURIComponent(
+          projectId,
+        )}/repository/branches/${encodeURIComponent(ref)}?` +
+          querystring.stringify({
+            ref,
+          }),
+        {
+          baseUrl: getBaseUrl(options),
+          headers: {
+            "Private-Token": options.privateToken,
+          },
+          json: true,
+          timeout: options.timeout,
         },
-        json: true,
-        timeout: options.timeout,
-      },
-    );
-    return {
-      body,
-      headers: pick(headers, basePickHeaders),
-    };
-  },
-});
+      );
+      return {
+        body,
+        headers: pick(headers, basePickHeaders),
+      };
+    },
+  };
+};
 
 // Utils
 
